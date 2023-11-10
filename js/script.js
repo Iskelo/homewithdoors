edaaa1027b0a9c4e61cf9ac6226386d803284a49
Iskelo <iskelo.kap@gmail.com>
2023-11-10 10:17:51 +0100

    opening

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
-const element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
+const elements = document.querySelectorAll(".door");
 
-function toggleDoor() {
-  element.classList.toggle("doorOpen");
+elements.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    element.classList.toggle(`doorOpen${getDirection(index)}`);
+  });
+});
+
+function getDirection(index) {
+  switch (index) {
+    case 0: return "Left";
+    case 1: return "Right";
+    case 2: return "Top";
+    case 3: return "Bottom";
+    default: return "Right";
+  }
 }
+
+// elements.forEach(element => {  
+//   element.addEventListener("click", ()=>{
+//     element.classList.toggle("doorOpenLeft");
+//   });
+// });
+
+//  console.log(elements)
+// for (let i = 0; i < elements.length; i++) {
+//   // const element = elements[i];
+ 
+//   elements[i]
+// }
