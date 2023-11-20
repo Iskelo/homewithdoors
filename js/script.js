cd5d1176f9dbab80e68b580688982a217d5407fd
Iskelo <iskelo.kap@gmail.com>
2023-11-20 13:02:51 +0100

    media

--- js/script.js
+++ js/script.js
@@ -1,43 +1,5 @@
 'use strict'; 
-/* const elements = document.querySelectorAll(".door");
 
-elements.forEach((element, index) => {
-  element.addEventListener("mouseenter", (event) => {
-    event.stopPropagation(); 
-    const direction = getDirection(index);
-    const isOpen = element.classList.contains(`doorOpen${direction}`);
-    closeAllDoors();
-    if (!isOpen) {
-      element.classList.add(`doorOpen${direction}`);
-    }
-  });
-  // element.addEventListener("mouseleave", () => {
-  //   closeAllDoors();
-  // });
-});
-
-document.addEventListener("click", (event) => {  
-  if (!event.target.closest(".door")) {
-    closeAllDoors();
-  }
-});
-
-function getDirection(index) {
-  switch (index) {
-    case 0: return "Left";
-    case 1: return "Right";
-    case 2: return "Top";
-    case 3: return "Bottom";
-    default: return "Right";
-  }
-}
-
-function closeAllDoors() {
-  elements.forEach((element, index) => {
-    const direction = getDirection(index);
-    element.classList.remove(`doorOpen${direction}`);
-  });
-} */
 const elements = document.querySelectorAll(".box");
 
 elements.forEach((element, index) => {
@@ -50,9 +12,6 @@ elements.forEach((element, index) => {
       element.classList.add(`doorOpen${direction}`);
     }
   });
-  // element.addEventListener("mouseleave", () => {
-  //   closeAllDoors();
-  // });
 });
 
 document.addEventListener("click", (event) => {  
